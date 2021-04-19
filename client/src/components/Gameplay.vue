75d3f19b26c6a1d1e6681d36ba6aee244cfa3da7
Piotr <pigryko@yahoo.com>
2021-04-19 12:39:50 +0100

    Gameplay working

--- client/src/components/Gameplay.vue
+++ client/src/components/Gameplay.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-      <button @click="getCurrentAnswers(); getCurrentQuestion();">Get Answers</button>
+      <button @click="getCurrentAnswers(indexCounter); getCurrentQuestion(indexCounter);">Get Answers</button>
+      <div>
+      <ul>
+          <li>
+          </li>
+      </ul>
+      </div>
+      <div>
+          <h2 v-html="currentQuestion">{{currentQuestion}}</h2>
+          <ul v-for="(answer, index) in currentAnswers" :key="index">
+              <li @click="checkAnswer(answer)">
+                 <button v-html="answer.answer">{{answer.answer}}</button>
+             </li>
+          </ul>
+      </div>
   </div>
 </template>
 
@@ -14,9 +28,11 @@ data() {
         questions: [],
         currentQuestion: null,
         currentAnswers: [],
-        moneyList: [100, 200, 300, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 125000, 250000, 500000, 1000000]
+        moneyList: [100, 200, 300, 500, 1000, 2000, 4000, 8000, 16000, 32000, 64000, 125000, 250000, 500000, 1000000],
+        indexCounter: 0
     }
 },
+
 mounted() {
     this.getAllQuestions()
 
@@ -40,22 +56,33 @@ methods: {
     },
 
     getCurrentQuestion: function(index) {
-        this.currentQuestion = this.questions[0].question
+        this.currentQuestion = this.questions[index].question
         console.log(this.currentQuestion)
     },
 
     getCurrentAnswers: function(index) {
         let answers = []
-        const answersWrong = this.questions[0].incorrect_answers // [answer, answer, ...]
+        const answersWrong = this.questions[index].incorrect_answers // [answer, answer, ...]
         answersWrong.forEach( incorrectAnswer => {
                 const fullAnswer = {answer: incorrectAnswer, correct: false}
                 answers.push(fullAnswer)       
         })
-        const answerCorrect =this.questions[0].correct_answer
+        const answerCorrect =this.questions[index].correct_answer
         answers.push({answer: answerCorrect, correct: true})
 
         let shuffledArray = shuffle(answers)
         this.currentAnswers = shuffledArray
+    },
+
+    checkAnswer: function(answer) {
+        if (answer.correct && this.indexCounter < 14) {
+            this.indexCounter ++;
+            this.getCurrentQuestion(this.indexCounter)
+            this.getCurrentAnswers(this.indexCounter)
+        }
+        else {
+            return
+        }
     }
  
 }
