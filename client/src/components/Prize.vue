86cfe72098fb0981046916d81af67d2a5cca6df6
Piotr <pigryko@yahoo.com>
2021-04-21 21:03:41 +0100

    Zsolt added

--- client/src/components/Prize.vue
+++ client/src/components/Prize.vue
@@ -1,10 +1,33 @@
 <template>
-  <h1>Coming Soon...</h1>
+  <div>
+      <img v-if="zsolt===false" :src="require(`../../../client/public/images/ludo.png`)" alt="">
+      <h1 v-if="zsolt===true">I DON'T THINK SO!!!</h1>
+      <img v-if="zsolt===true" :src="require(`../../../client/public/images/zsoltfinger.gif`)" alt="">
+      <button v-if="zsolt===false" @click="playLudo">Place a nice game of Ludo</button>
+      <button v-if="zsolt===true" @click="goHome">TRY AGAIN</button>
+  </div>
+  
+  
 </template>
 
 <script>
+import { eventBus } from '@/main.js'
+
 export default {
-  name: "prize"
+  name: "prize",
+   data() {
+    return {
+      zsolt: false
+    }
+  },
+  methods: {
+        playLudo: function() {
+            this.zsolt = true
+        },
+        goHome: function() {
+            eventBus.$emit('go-home')
+        }
+  }
 }
 </script>
 
