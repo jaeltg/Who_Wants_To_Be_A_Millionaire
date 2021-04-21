82e29c8c051d6fc668672f5deec1ecd193868c87
Dani <danikav1989@gmail.com>
2021-04-21 11:59:46 +0100

    refactoring complete

---
+++ client/src/components/MoneyList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+       <ul v-for="(money, index) in moneyList.slice().reverse()" :key="index" id="money-list">
+          <li v-if="potentialPrize === money" class="highlight">
+              £{{money}}
+              </li>
+             <li v-else class="normal"> <p>£{{money}}</p>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'money-list',
+    props: ['moneyList', 'potentialPrize']
+}
+</script>
+
+<style scoped>
+#moneyList > li > p {
+    font-family: Copperplate;
+}
+.highlight {
+    background-color: orange;
+}
+</style>
